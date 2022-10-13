efa4e93661e74d853eb3e630645a147ebe6567b4
Maciej Palmowski <m.palmowski@freshpixels.pl>
2022-10-13 15:58:34 +0200

    env variables or .env files

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -2,8 +2,9 @@
 // phpcs:ignoreFile
 
 use Symfony\Component\Dotenv\Dotenv;
-
-(new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../.env', 'dev', ['test'], true);
+if ( file_exists( DRUPAL_ROOT . '/../.env' ) ) {
+  (new Dotenv())->usePutenv()->bootEnv(DRUPAL_ROOT . '/../.env', 'dev', ['test'], true);
+}
 
 /**
  * @file
